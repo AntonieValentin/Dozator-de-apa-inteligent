44f2458279bb0449c95a98473be8f0663289ac63
Aureliu Valentin Antonie <antonieaureliuvalentin@gmail.com>
2026-05-19 21:13:14 +0300

    calibrare senzor greutate

--- Proiect PM/src/main.cpp
+++ Proiect PM/src/main.cpp
@@ -1,19 +1,44 @@
-#include <Wire.h>
-#include <LiquidCrystal_I2C.h>
+#include "HX711.h"
 
-LiquidCrystal_I2C lcd(0x27, 16, 2); 
+// pini HX711
+const int LOADCELL_DOUT_PIN = A2;
+const int LOADCELL_SCK_PIN = A3;
+
+HX711 scale;
+
+float calibration_factor = 100.0; 
 
 void setup() {
-  Wire.begin();
-  lcd.init();
-  lcd.backlight();
-  
-  lcd.setCursor(0, 0);
-  lcd.print("Ecranul");
-  lcd.setCursor(0, 1);
-  lcd.print("functioneaza!");
+  Serial.begin(9600);
+  Serial.println("Asteapta");
+
+  scale.begin(LOADCELL_DOUT_PIN, LOADCELL_SCK_PIN);
+
+  scale.set_scale(calibration_factor);
+  scale.tare(); 
+
+  Serial.println("Pune sticla");
 }
 
 void loop() {
-	
+  scale.set_scale(calibration_factor);
+
+  Serial.print("Greutate citita: ");
+  Serial.print(scale.get_units(10), 1); 
+  Serial.print(" grame");
+  Serial.print("  |  Factor curent: ");
+  Serial.println(calibration_factor);
+
+  if (Serial.available()) {
+    char temp = Serial.read();
+    if (temp == '+' || temp == 'a') {
+      calibration_factor += 10;
+    } else if (temp == '-' || temp == 'z') {
+      calibration_factor -= 10;
+    } else if (temp == 'A') {
+      calibration_factor += 100;
+    } else if (temp == 'Z') {
+      calibration_factor -= 100;
+    }
+  }
 }
